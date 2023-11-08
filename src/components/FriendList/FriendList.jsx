41b09ae4fb600e3129e36926e56dbab8ec08a767
MagdalenaPezacka <magdalena.pezacka.it@gmail.com>
2023-11-08 22:42:29 +0100

    statistics

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,22 @@
+import css from './FriendList.module.css';
+import PropTypes from 'prop-types';
+
+const FriendList = ({ friends }) => {
+  console.log(friends);
+  //   return (
+  //     <ul class="friend-list">
+  //       <li class="item">
+  //         <span class="status"></span>
+  //         <img class="avatar" src="" alt="User avatar" width="48" />
+  //         <p class="name"></p>
+  //       </li>
+  //     </ul>
+  //   );
+};
+
+FriendList.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
+export default FriendList;
